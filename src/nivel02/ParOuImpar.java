2c0867d84349c93854d4ff2076ae6a8042bc8c2b
Vanderson Andrade <vandersonandr@gmail.com>
2026-06-09 09:45:19 -0300

    feat: resolve exercicio par ou impar usando operador ternario

---
+++ src/nivel02/ParOuImpar.java
@@ -0,0 +1,19 @@
+package nivel02;
+
+import java.util.Scanner;
+
+public class ParOuImpar {
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Digite um número para saber se é par ou impar: ");
+        int numero = in.nextInt();
+        int numeroTeste = numero % 2;
+
+        String resultado = (numeroTeste == 0)?"O número é par":"O número é impar";
+
+        System.out.println(resultado);
+        in.close();
+    }
+}
